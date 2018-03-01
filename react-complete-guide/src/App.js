05c55629c296d833b8a94ef1ad3f064f0656d5ad
Ian Nicholson <ipnicholson@gmail.com>
2018-03-01 11:07:10 -0800

    progress commit

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,27 +5,43 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      {name: 'Ian', age: 99},
-      {name: 'Steelhead', age: 14},
-      {name: 'Stephanie', age: 26}
+      {id: '1', name: 'Ian', age: 99},
+      {id: '2', name: 'Steelhead', age: 14},
+      {id: '3', name: 'Stephanie', age: 26}
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
   togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons
+    const doesShow = this.state.showPersons;
     this.setState( {showPersons: !doesShow} ) // setState merges new state with old state
   }
 
-  nameChangeHandler = (event) => {
-    this.setState( {
-      persons: [
-        {name: 'Ian', age: 99},
-        {name: event.target.value, age: 14},
-        {name: 'Stephanie', age: 27}
-      ]
-    } )
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex] // use spread operator to avoid mutating state
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} );
+  }
+
+  deletePersonHandler = (personIndex) => {
+    console.log('person clicked')
+    // const persons = this.state.persons.slice(); // slice is used to create a copy of persons instaed of a pointer (slice w/o args simply copies array)
+    const persons = [...this.state.persons]; // Use ES6 spread operator to copy immutably
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   render() {
@@ -35,17 +51,20 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-    }
+    };
 
     let persons = null;
 
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map( person => {
+          {this.state.persons.map((person, index) => { // When using more than one arg in ES6 arrow functions, you must wrap them with ()
             return <Person
+              click={() => this.deletePersonHandler(index)} // execute as an arrow function, alternative is bind
               name={person.name}
-              age={person.age} />
+              age={person.age}
+              changed={(event) => this.nameChangeHandler(event, person.id)}
+              key={person.id} />
           })}
         </div>
       );
@@ -63,7 +82,6 @@ class App extends Component {
         {persons}
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, `Hi! I'm a nested element!`)) // Using calls to React.createElement instead of calling return() with JSX as an argument
   }
 }
 
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,10 +3,10 @@ import './Person.css'
 
 const Person = (props) => {
   return (
-      <div className="Person">
+      <div className="Person" >
         <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
+        <input type="text" onChange={props.changed} value={props.name} />
       </div>
     )
 }
