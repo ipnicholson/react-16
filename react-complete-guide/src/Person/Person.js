9d1c9b8eca4906793775466d27b488c2c92b7fe8
Ian Nicholson <ipnicholson@gmail.com>
2018-03-02 15:25:05 -0800

    throw an error at random

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './Person.css'
 
 const Person = (props) => {
+  const rnd = Math.random();
+
+  if (rnd > 0.7) {
+    throw new Error('Something went wrong');
+  }
+
   return (
       <div className="Person" >
         <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
