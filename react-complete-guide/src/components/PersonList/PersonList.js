4c9b3bd930747e10d80b2048052b7d3d94db3151
Ian Nicholson <ipnicholson@gmail.com>
2018-03-05 13:10:16 -0800

    convert to stateful component

--- react-complete-guide/src/components/PersonList/PersonList.js
+++ react-complete-guide/src/components/PersonList/PersonList.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Person from './Person/Person'
 
-const PersonList = (props) => ( props.persons.map( ( person, index ) => {
-    return <Person
-      click={() => props.click( index )} // execute as an arrow function, alternative is bind
-      name={person.name}
-      age={person.age}
-      changed={(event) => props.changed( event, person.id )}
-      key={person.id}
-    />
-  })
-);
+class PersonList extends Component  {
+  render () {
+    return this.props.persons.map( ( person, index ) => {
+      return <Person
+        click={() => this.props.click( index )} // execute as an arrow function, alternative is bind
+        name={person.name}
+        age={person.age}
+        changed={(event) => this.props.changed( event, person.id )}
+        key={person.id}
+      />
+    })
+  }
+}
 
 export default PersonList
